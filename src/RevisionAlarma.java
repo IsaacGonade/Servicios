62ed15b035ab5a3bc12fdd61db36506cac2ec270
IsaacGonade <isaac.gonade@educa.jcyl.es>
2024-02-23 08:36:16 +0100

    INICIO

--- src/RevisionAlarma.java
+++ src/RevisionAlarma.java
@@ -1,10 +1,9 @@
 import java.time.LocalDate;
 
 public class RevisionAlarma extends Servicio{
-
     int numAlarmasRevisar;
 
-    public RevisionAlarma(String cliente, int numAlarmasRevisar) {
+    public RevisionAlarma(String trabajador, LocalDate fechaInicio, String cliente, int numAlarmasRevisar) {
         super(cliente);
         this.numAlarmasRevisar = numAlarmasRevisar;
         this.Trabajador = "Revisor Especialista Contraincendios";
